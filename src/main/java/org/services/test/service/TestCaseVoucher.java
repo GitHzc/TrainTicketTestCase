757ccfdef47fe7b650ea6d9a9ad2ce0792a6c905
kylin xiang <15884286060@163.com>
2018-10-12 21:04:51 +0800

    first push

---
+++ src/main/java/org/services/test/service/TestCaseVoucher.java
@@ -0,0 +1,8 @@
+package org.services.test.service;
+
+import org.springframework.web.client.RestTemplate;
+
+public class TestCaseVoucher {
+    RestTemplate restTemplate = new RestTemplate();
+    
+}
